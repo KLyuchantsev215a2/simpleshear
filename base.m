b92a4e77377c9e973d528e3b10820e38fb1babb9
Vladislav <klychik-nsk54rus@mail.ru>
2018-11-12 21:19:03 +0700

    Добавлено вычисление второй производной ядра для стабилизации
    Нужно сделать вычисление гессиана? смешанные производные равны нулю?
    Как стабилизировать численное решение?

--- base.m
+++ base.m
@@ -37,22 +37,34 @@ SIG=ComputeStress(F,mu,k,N);
 
     
 for n = 1:fix(Time/dt)
+    
     W_cor=zeros(N,N);
     W_cor_1per=zeros(N,N);
     W_cor_2per=zeros(N,N);
     nabla_W_cor=zeros(2,N,N);
+    Hessian_W_cor=zeros(2,N,N);
     W_cor=ComputeW_cor(N,x,x,V,h);
+    
     x_per1=x;
     x_per2=x;
+    x_per1_inv=x;
+    x_per2_inv=x;
     
     x_per1(1,1:N)=x_per1(1,1:N)+dh;
     x_per2(2,1:N)=x_per2(2,1:N)+dh;
+    x_per1_inv(1,1:N)=x_per1(1,1:N)-dh;
+    x_per2_inv(2,1:N)=x_per2(2,1:N)-dh;
     
     W_cor_1per=ComputeW_cor(N,x,x_per1,V,h);
     W_cor_2per=ComputeW_cor(N,x,x_per2,V,h);
+    W_cor_1per_inv=ComputeW_cor(N,x,x_per1_inv,V,h);
+    W_cor_2per_inv=ComputeW_cor(N,x,x_per2_inv,V,h);
     
     nabla_W_cor(1,1:N,1:N)=(W_cor_1per-W_cor)/dh;
     nabla_W_cor(2,1:N,1:N)=(W_cor_2per-W_cor)/dh;
+    
+    Hessian_W_cor(1,1:N,1:N)=(W_cor_1per-2*W_cor+W_cor_1per_inv)/(dh*dh);
+    Hessian_W_cor(2,1:N,1:N)=(W_cor_2per-2*W_cor+W_cor_2per_inv)/(dh*dh);
     
     if (n==1)
         nabla_W_cor_0=nabla_W_cor;
